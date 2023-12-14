71574aad406445b28255612a515ff14cd466d6a9
BiancaBarcelos <biancassbarcelos@gmail.com>
2023-12-13 21:42:57 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
+
 alert ("Insira dois números e veja qual é o maior")
-let num1 = parseInt (prompt ("Por favor, insira o primeiro número"));
-let num2 = parseInt(prompt ("Por favor, insira o segundo número"));
+let num1 = prompt ("Por favor, insira o primeiro número");
+let num2 = prompt ("Por favor, insira o segundo número");
 
-
-function numero (num1, num2){   
-
-    const num1Maior = alert (`O número ${num1} é maior que ${num2}`);
-    const num2Maior = alert (`O número ${num2} é maior que ${num1}`);
-    const numIgual = alert (`São os mesmos valores!!`);
+function compararNum (num1, num2){
     
-    if (num1 > num2) {
-        return num1Maior;
+    
+    if(num1 > num2) {
+        return alert (`O número ${num1} é maior que ${num2}`);
     }else if (num2 > num1){
-        return num2Maior;
-    } else if (num1 == num2) {
-        return numIgual;
+        return alert (`O número ${num2} é maior que ${num1}`);
+    }else if (num1 == num2) {
+        return alert ("São os mesmos valores!!");
     }
     
 }
 
-numero (num1, num2);
+compararNum (num1, num2);
