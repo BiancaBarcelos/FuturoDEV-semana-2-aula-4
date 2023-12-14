dd7d6b94168e9be00b088e1d3642fac68db42537
BiancaBarcelos <biancassbarcelos@gmail.com>
2023-12-13 22:11:10 -0300

    adicao de logo e css

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-
 alert ("Insira dois números e veja qual é o maior")
 let num1 = prompt ("Por favor, insira o primeiro número");
 let num2 = prompt ("Por favor, insira o segundo número");
 
 function compararNum (num1, num2){
-    
-    
+   
     if(num1 > num2) {
-        return alert (`O número ${num1} é maior que ${num2}`);
-    }else if (num2 > num1){
-        return alert (`O número ${num2} é maior que ${num1}`);
-    }else if (num1 == num2) {
-        return alert ("São os mesmos valores!!");
+        return `O número ${num1} é maior que ${num2}`;
     }
     
+    if (num2 > num1){
+        return `O número ${num2} é maior que ${num1}`;
+    }
+    
+    if (num1 == num2) {
+        return "São os mesmos valores!!";
+    }  
 }
 
-compararNum (num1, num2);
+const retorno = compararNum (Number(num1), Number(num2));
+alert(retorno);
